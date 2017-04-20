a10d0868e0b68a9f3d3adff1efa78263b827d6cd
Asia Wasilewska <asia_wasilewska@tlen.pl>
2017-04-20 23:10:23 +0200

    added comment

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name);	/*komentarz blokowy*/
+console.log('Witaj, ' + name); /*komentarz blokowy*/
+//komentarz liniowy
